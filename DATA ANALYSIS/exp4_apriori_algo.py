628b1e48849853ffa7c7ed1814bd76a30cde8057
Shaheen <84270559+shaheen-senpai@users.noreply.github.com>
2022-10-24 15:01:32 +0530

    Update exp4_apriori_algo.py
    note i used combinations and brute forced the algorithm thus it will not have a good time complexity and can't handle really big data sets.

--- DATA ANALYSIS/exp4_apriori_algo.py
+++ DATA ANALYSIS/exp4_apriori_algo.py
@@ -1,5 +1,5 @@
 #Apriori Algorithm
-
+from itertools import combinations
 def union(lst1, lst2):    #list join operation
     final_list = list(set(lst1) | set(lst2))
     final_list.sort()     #lexiographical order
@@ -22,23 +22,32 @@ for i in range(1,ii): items = union(items,data[keys[i]]) #ii = 9
 min_support = int(input("Enter the minimum support "))
 
 c = {}
+L=1
 for x in items:
     count=0
     for i in range(1,ii):
         if(x in data[keys[i]] ):
             count+=1
-    c[x]=count
+    if(count>=min_support):
+        c[x]=count
+print("Frequent %d-itemsets are: " % L)
+for i in c:
+    print(i,c[i])
 
-while(len(c)>0):
-
-    for i in c:
-        if(c[i] < min_support): c.pop(i)  #pruned
-    
-    break
-
-        
-print(data)
-print(items)
-print(c)
-
-
+while(len(c)!=0):
+    L+=1
+    c1 = list(combinations(items,L))
+    #print(c1)
+    c={}
+    for x in c1:
+        count=0
+        for i in range(1,ii):
+            #print(set(x),set(data[keys[i]]))
+            if(set(x)<=set(data[keys[i]])):
+                count+=1
+        if(count>=min_support):
+            c[x]=count
+    if(len(c)!=0):
+        print("Frequent %d-itemsets are: " % L)
+        for i in c:
+            print(i,c[i])
