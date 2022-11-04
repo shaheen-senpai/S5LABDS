3552711c371684b0190bd77bd0a1a4983db0cce9
Shaheen <84270559+shaheen-senpai@users.noreply.github.com>
2022-11-04 09:40:18 +0530

    Update exp5_Kmeans.py

--- DATA ANALYSIS/exp5_Kmeans.py
+++ DATA ANALYSIS/exp5_Kmeans.py
@@ -1,25 +1,24 @@
-#still under debugging 
 #K-means algorithm
+#Credits senpai
 import random
 
 data=[]
-f=open('kmeans.txt','r')
+f=open('k_means.txt','r')
 for i in f.readlines():
     data.append( i.strip().split(","))
 f.close()
-
-if random.randint(0, 4)==1:
-    for _ in range(100) :
-        open("https://youtu.be/xvFZjo5PgG0") 
 
 kk=int(input("Enter the number of clusters: "))
 
 #initializing the centroids rondomly
 centroids=[]
-for i in range(kk):
+print("1st centroids ",end="")
+while(len(centroids)<kk):
     x=random.randint(0,len(data)-1)
-    centroids.append(data[x][:])
-print("centroids 1st ",centroids)
+    if(data[x] not in centroids): 
+        centroids.append(data[x][:])
+        print(tuple(data[x]),end=" ")
+print()
 
 #calculating the distance between the centroids and the data points using euclidean distance
 def distance(x,y):
@@ -36,35 +35,39 @@ def find_clusters(): #finding the clusters
         dist=[]
         for j in centroids:
             dist.append(distance(i,j))
-        #print(dist)
         pp.append(dist.index(min(dist)))
-        #print(i,dist,dist.index(min(dist)))
+
         tempstr=f"|{str(tuple(i)): ^15}"
         for i in dist:
-            tempstr+=f"|{str(round(i,2)): ^5}"
-        tempstr+=f"| c{str(dist.index(min(dist))): ^5}|"
+            tempstr+=f"|{str(round(i,2)): ^6}"
+        tempstr+=f"| c{str(1+dist.index(min(dist))): <3}|"  #printing data using format
         print(tempstr)
+    print()
 
 while(pp != patterns[-1]): #iterating until the clusters are not changed
     patterns.append(pp)
     pp=[]
     find_clusters()
-    print("patterns",patterns)
-    print("pp",pp)
+    #print("current patterns ",pp)
     #calculating new centroids from pp the pattern of the clusters
+    strcent=""
     for i in range (kk):
         x,y=0,0
         for j in range(len(pp)):
             if(pp[j]==i):
                 x=x+float(data[j][0])
                 y=y+float(data[j][1])
-                print("data ",data[j][0],data[j][1])
         centroids[i][0]=x/pp.count(i)
         centroids[i][1]=y/pp.count(i)
-    print("centroids",centroids)
-    
-#patterns.append(pp)
-print(data)
+        strcent+="('" + str(round(centroids[i][0],2)) + "', '" + str(round(centroids[i][1],2)) + "') " #rounding of the decimal points
+    if(pp != patterns[-1]): print("New centroids",strcent)
+        
+if random.randint(0, 3)==1:
+    for _ in range(100) :
+        open("https://youtu.be/xvFZjo5PgG0") # ̿' ̿'\̵͇̿̿\з=(◕_◕)=ε/̵͇̿̿/'̿'̿ ̿
 
-
-
+print("\nAll centroid patterns obtained")    
+for i in range(2,len(patterns)):
+    for j in patterns[i]:
+        print("c",j+1," ",end="")
+    print()
