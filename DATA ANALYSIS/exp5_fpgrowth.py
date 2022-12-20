0ebc51e07911046a3dce1dcb41393b6c6a05e1a1
Shaheen <84270559+shaheen-senpai@users.noreply.github.com>
2022-12-20 23:49:00 +0530

    Update exp5_fpgrowth.py

--- DATA ANALYSIS/exp5_fpgrowth.py
+++ DATA ANALYSIS/exp5_fpgrowth.py
@@ -1,5 +1,5 @@
 #FP-Growth Algorithm
-
+#credits senpai ;) gimme a thumbs up if it was helpful 👍
 def union(lst1, lst2):    #list join operation
     final_list = list(set(lst1) | set(lst2))
     final_list.sort()     #lexiographical order
@@ -22,7 +22,7 @@ for i in f.readlines():
 keys=list(data.keys())  #transaction Id's
 
 for i in range(1,ii): items = union(items,data[keys[i]]) #ii = 9
-min_support = int(input("Enter the minimum support ")) 
+min_support = int(input("\nEnter the minimum support ")) 
 
 #creating L1 and priority set for fp growth tree 
 c = []
@@ -39,6 +39,7 @@ prio=[i[0] for i in c]
 
 for i in range(1,ii):
     data[keys[i]].sort(key=lambda x:prio.index(x)) #sorts data according to prio
+print("\n************************* Sorted data according to priority set *************************\n")
 for i in data: print(i,data[i])
 
 #creating fp tree class
@@ -77,10 +78,14 @@ def traverse(root,lvl):
         return
     for i in range(0,lvl):
         print("\t",end="")
-    print(root.name,root.count)
+    if(lvl == 0):
+        print(root.name)
+    else:
+        print(root.name,root.count)
     for i in root.child:
         traverse(root.child[i],lvl+1)
 
+print("\n***************************   FP Tree   ***************************\n")
 traverse(start,0)
 
 #creating conditional pattern base
@@ -98,21 +103,39 @@ def cond_pattern_base(root,i):
         while traverse.parent.name != 'root':
             cpb[i][-1].append(traverse.parent.name)
             traverse = traverse.parent
+        if(len(cpb[i][-1]) == 1):
+            cpb[i].pop()
     for j in root.child:
         cond_pattern_base(root.child[j],i)
 
 for i in prio:
     cond_pattern_base(start,i)
+print("\n*************************  Conditional Pattern Base  *************************\n")
+for i in cpb:
+    print(i,' -> ',cpb[i])
+print()
 
-for i in cpb:
-    print(i,cpb[i])
-    
+freq_patterns = [] #list of frequent patterns
+def freq_itemsets(root):
+    if root == None:
+            return
+    if root.child == {}:
+        traverse = root
+        a={}
+        while traverse.name != r:
+            a[traverse.name] = traverse.count
+            traverse = traverse.parent
+        freq.append(a)
+        return
+    for j in root.child:  
+        freq_itemsets(root.child[j])
+
 #creating conditional fp tree
-for i in cpb:
+for r in cpb:
     root = Node()
     st = root
-    root.name = i
-    for j in cpb[i]:
+    root.name = r
+    for j in cpb[r]:
         current = root
         for k in j[1::1]:
             if k in current.child:
@@ -128,10 +151,41 @@ for i in cpb:
                 current.parent = root
                 root = current
         root = st
-    traverse(root,0)
-    
-    
+    print("\n*************************  Conditional FP Tree for ",r,"  *************************\n")
+    traverse(st,0)
+    #creating frequent itemsets
+    freq = []
+    freq_itemsets(st)
+    print(freq,'\n')
+    for i in freq:
+        #print(i.keys())
+        from itertools import combinations
+        for j in range(1,len(i.keys())+1):
+            for k in combinations(i.keys(),j):
+                count=[]
+                for x in k:
+                    count.append(i[x])
+                    #print(x,i[x])
+                #print(k,min(count))
+                if min(count) >= min_support:
+                    l = list(k)
+                    l.append(r)
+                    l.append(min(count))
+                    freq_patterns.append(l)
+                    #print(l)
+print("\n*************************  Frequent Patterns  *************************\n")
+#print(freq_patterns)
 
-            
-            
-        
+#merging duplicate patterns
+for i in range(0,len(freq_patterns)):
+    for j in range(i+1,len(freq_patterns)):
+        if(freq_patterns[i][0:-1] == freq_patterns[j][0:-1]):
+            freq_patterns[i][-1] += freq_patterns[j][-1]
+            freq_patterns[j] = []
+freq_patterns = [i for i in freq_patterns if i != []]
+
+#sorting patterns according to length
+freq_patterns.sort(key = len)
+
+for i in freq_patterns:#printing frequent patterns
+    print(i)
